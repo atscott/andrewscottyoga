ef4ffd67565a14a8c8294200b2083356f3885e36
Andrew Scott <atscott01@gmail.com>
2017-07-14 14:02:08 -0700

    headless chrome and single run

--- karma.conf.js
+++ karma.conf.js
@@ -27,7 +27,8 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
+    browsers: ['ChromeHeadless'],
+    concurrency: Infinity
+    //singleRun: false
   });
 };
